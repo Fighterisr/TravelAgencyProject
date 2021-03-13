122ae279258f4484c5f69596bc569a2b825f2d81
Omri Biton <fighterisr@gmail.com>
2021-03-14 00:26:36 +0200

    trying to fix heroku #2

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const port = process.env.PORT || 3000;
 const app = express();
+const path = require('path');
 
 app.use(express.static('src'));
 
-app.get('*', (req, res)=> {
-    res.sendFile(__dirname + '/Home.html');
+app.get('/', (req, res)=> {
+    res.sendFile(path.join(__dirname, '/Home.html'));
 
 });
 
 app.listen(port, ()=>{
     console.log(`server is up and running at http://127.0.0.1:${port}`);
+    console.log(__dirname);
 });
